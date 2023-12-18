eb8f617be597ab40fe3225f8d07923ae37c72457
Davi Lhlapak <davilhlapak@hotmail.com>
2023-12-18 16:18:12 -0300

    chore: export jwt types

--- index.ts
+++ index.ts
@@ -2,5 +2,14 @@ import { getMiddleware } from './src/middleware'
 import { getSession } from './src/session'
 import { getApiRoutes } from './src/api'
 import { signIn, signOut } from './src/auth'
+import { JWT, DefaultJWT } from './src/jwt'
 
-export { getMiddleware, getSession, getApiRoutes, signIn, signOut }
+export {
+    getMiddleware,
+    getSession,
+    getApiRoutes,
+    signIn,
+    signOut,
+    JWT,
+    DefaultJWT
+}
